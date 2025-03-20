87f5816c45b54da40f04ceae3ee374f6ca63b114
AtulMinz <atulminz.jsr@gmail.com>
2025-03-20 07:59:53 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import axios from "axios";
+import { config } from "dotenv";
